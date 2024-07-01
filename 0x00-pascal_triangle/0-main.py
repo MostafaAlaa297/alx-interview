73c358b4c4038f9c5a0abb42bcebd3f7963482ec
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-01 02:50:14 -0700

    fix: update pascal

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -5,7 +5,7 @@
 pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
-        """Print the triangle"""
+    """Print the triangle"""
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,9 +7,7 @@
 """
 
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing the Pascal’s triangle of n
-    """
+    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
         return []
     triangle = [[1]]
@@ -18,6 +16,6 @@ def pascal_triangle(n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        triangle.append(1)
+        row.append(1)
         triangle.append(row)
     return triangle
