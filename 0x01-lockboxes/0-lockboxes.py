657ea18272c48f4a06305259e067cacd7341033e
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-07 15:24:37 -0700

    finish task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Parameters:
+    boxes (list of lists): List of boxes where each box contains keys to other boxes.
+
+    Returns:
+    bool: True if all boxes can be opened, otherwise False.
+    """
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    keys = {0}
+    opened_boxed = Set()
+
+    while keys:
+        new_key = keys.pop()
+
+        if new_key not in opened_boxes:
+            opened_boxes.add(new_key)
+            
+            for key in boxes[new_key]:
+                if key < n:
+                    keys.add(key)
+    return len(opened_boxes) === n
