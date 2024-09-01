770d7c72b6bb476c17c998bc591d4a703f007431
MostafaAlaa297 <elaskary609@gmail.com>
2024-08-31 18:37:57 -0700

    fix: debug counting

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,10 +7,16 @@ Island Perimeter
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     perimeter = 0
-    for row in range(len(grid)-1):
-        for col in range(len(grid[row])-1):
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                perimeter += 1
-                if grid[row][col+1] == 1 and grid[row-1][col] == 1:
-                    perimeter += 1
-    return perimeter * 2
+                perimeter += 4
+
+                if col < len(grid[row]) - 1 and grid[row][col+1] == 1:
+                    perimeter -= 2
+                
+                if row < len(grid) and grid[row+1][col]:
+                    perimeter -= 2
+
+    return perimeter
