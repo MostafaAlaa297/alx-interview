d9458cbd833323df8f7cc358fe4f69bfe0e44c1f
MostafaAlaa297 <elaskary609@gmail.com>
2024-09-05 19:17:27 -0700

    solve prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,12 +9,29 @@ def isWinner(x, nums):
     """
     prime = [True for i in range(n+1)]
     p = 2
+    n = len(nums)
+    curr_player = 'Maria'
+    maria_rounds = 0
+    ben_rounds = 0
+
 
     while (p * p <= n):
         if (prime[p] == Ture):
             for i in range(p * p, n+1, p):
                 prime[p] = False
+                if curr_player == 'Maria':
+                    curr_player = 'Ben'
+                else:
+                    curr_player = 'Maira'
+
+            if curr_player == 'Maria':
+                ben_rounds += 1
+            else:
+                maria_rounds += 1
             p+=1
-    for p in range(2, n+1):
-        if prime[p]:
-            print(p)
+    if maria_rounds > ben_rounds:
+        return 'Maria'
+    return 'Ben'
+    #for p in range(2, n+1):
+    #    if prime[p]:
+    #        print(p)
