8520a6cbb020f6ccdb4728037cc1248fcce18608
MostafaAlaa297 <elaskary609@gmail.com>
2024-09-06 04:33:23 -0700

    fix: debug code and edit style

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,20 +3,20 @@
 Prime Game
 """
 
+
 def isWinner(x, nums):
     """
     Return the winner of prime game competetion
     """
+    n = len(nums)
     prime = [True for i in range(n+1)]
     p = 2
-    n = len(nums)
     curr_player = 'Maria'
     maria_rounds = 0
     ben_rounds = 0
 
-
     while (p * p <= n):
-        if (prime[p] == Ture):
+        if (prime[p] is True):
             for i in range(p * p, n+1, p):
                 prime[p] = False
                 if curr_player == 'Maria':
@@ -28,10 +28,7 @@ def isWinner(x, nums):
                 ben_rounds += 1
             else:
                 maria_rounds += 1
-            p+=1
+                p += 1
     if maria_rounds > ben_rounds:
         return 'Maria'
     return 'Ben'
-    #for p in range(2, n+1):
-    #    if prime[p]:
-    #        print(p)
